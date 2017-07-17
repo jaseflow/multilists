ae1faa6cc6c3a0554d374483dd90243d82f1f1ef
Jason Corbett <jcorbett@expedia.com>
2017-07-17 13:29:47 +1000

    Delete functionality

--- js/index.js
+++ js/index.js
@@ -3,13 +3,21 @@ var hearts = document.querySelectorAll(".heart")
     newCheckbox = document.getElementById("new-list-checkbox"),
     modalContent = document.getElementById("modal-content")
     toast = document.getElementById("toast"),
-    heartNav = document.getElementById("heart-nav");
+    heartNav = document.getElementById("heart-nav"),
+    toastActions = document.querySelectorAll(".toast__action"),
+    newInput = document.getElementById("new-list-input"),
+    checkboxTemplate = document.getElementById("checkbox-template");
 
 var titleSuggestion,
     activeHeartItemId;
 
 var totalCheckedLists = 0;
 
+function addListTemplate(list) {
+  checkboxTemplate.content.querySelector("span").textContent = list.dataset.listName;
+  checkboxTemplate.content.querySelector("input").setAttribute("id", list.dataset.listName.replace(/\s+/g, "-").toLowerCase());
+  document.getElementById("above-here").insertBefore(document.importNode(checkboxTemplate.content, true), null);
+}
 
 function handleListFormSubmit(e) {
   var lists = e.target.querySelectorAll("input"),
@@ -22,7 +30,13 @@ function handleListFormSubmit(e) {
     if(list.checked) {
       checked.push(list);
     }
+    if(list.checked && list.id === "new-list-checkbox") {
+      newInput.setAttribute("hidden", true);
+      newCheckbox.checked = false;
+      addListTemplate(list);
+    }
   });
+
   setTimeout(function() {
     saveToList(checked);
   });
@@ -35,31 +49,43 @@ function handleNewInputKeypress(e) {
 function saveToList(lists) {
   var singleListMessage = document.getElementById("single-list-message"),
       multipleListMessage = document.getElementById("multiple-list-message"),
+      removalListMessage = document.getElementById("removal-list-message"),
       singleListName = document.getElementById("single-list-name");
+      removalListName = document.getElementById("removal-list-name");
   if(lists.length > 0) {
     heartNav.classList.add("lists-link--on");
     toast.classList.add("toast--pop");
-    document.querySelector('input[data-item-id=' + activeHeartItemId + ']').checked = true;
-    setTimeout(function() {
-      toast.classList.add("toast--press");
-      setTimeout(function() {
-        toast.setAttribute("class", "toast");
-      }, 1000);
-    }, 3000);
     if(lists.length == 1) {
       singleListMessage.hidden = false;
       singleListName.textContent = lists[0].dataset.listName;
     } else {
       multipleListMessage.hidden = false;
     }
+    document.querySelector('input[data-item-id=' + activeHeartItemId + ']').checked = true;
+    setTimeout(function() {
+      toast.classList.add("toast--press");
+      setTimeout(function() {
+        toast.setAttribute("class", "toast");
+        singleListMessage.hidden = true;
+        multipleListMessage.hidden = true;
+      }, 1000);
+    }, 3000);
   } else if(lists.length === 0) {
+    toast.classList.add("toast--pop");
     document.querySelector('input[data-item-id=' + activeHeartItemId + ']').checked = false;
+    removalListMessage.hidden = false;
+    setTimeout(function() {
+      toast.classList.add("toast--press");
+      setTimeout(function() {
+        toast.setAttribute("class", "toast");
+        removalListMessage.hidden = true;
+      }, 1000);
+    }, 3000);
   }
 }
 
 function displayLists(suggestion) {
-  var newInput = document.getElementById("new-list-input"),
-      saveBtn = document.getElementById("lists-save-button"),
+  var saveBtn = document.getElementById("lists-save-button"),
       listOptions = modal.querySelectorAll(".lists__option"),
       listForm = modal.querySelector("#lists-form");
   if(!modal.classList.contains("modal--in")) {
@@ -71,7 +97,7 @@ function displayLists(suggestion) {
         newInput.removeAttribute("hidden");
         if(suggestion) {
           newInput.setAttribute("value", suggestion);
-          newInput.setAttribute("data-list-name", suggestion);
+          newCheckbox.setAttribute("data-list-name", suggestion);
         }
         newInput.focus();
         newInput.select();
@@ -79,7 +105,7 @@ function displayLists(suggestion) {
     })
   });
   listForm.addEventListener("submit", handleListFormSubmit);
-  newInput.addEventListener("keypress", handleNewInputKeypress);
+  newInput.addEventListener("keyup", handleNewInputKeypress);
 }
 
 hearts.forEach(function(heart) {
@@ -91,6 +117,14 @@ hearts.forEach(function(heart) {
     displayLists(suggestion, id);
   });
 })
+
+toastActions.forEach(function(action) {
+  action.addEventListener("click", function(e) {
+    var suggestion = e.target.dataset.suggestion,
+        id = e.target.dataset.itemId;
+    displayLists(suggestion + "2", id);
+  });
+});
 
 modal.addEventListener("click", function(e) {
   var listOptions = modal.querySelectorAll(".lists__option");
